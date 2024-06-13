fd8441e87853a4a2de7cf6407c15adb4a9ec491a
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-13 08:58:13 +0530

    book now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Viewdocs from './components/Viewdocs';
+import BookNow from './components/BookNow';
 
 function App() {
   return (
     <div>
       <Viewdocs/>
+      <BookNow/>
     </div>
   );
 }
