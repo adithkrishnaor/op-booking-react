c617df4f8611691dda587421f5a74f6e8daed1b2
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-13 09:14:37 +0530

    view booking created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Viewdocs from './components/Viewdocs';
 import BookNow from './components/BookNow';
+import ViewBooking from './components/ViewBooking';
 
 function App() {
   return (
     <div>
       <Viewdocs/>
       <BookNow/>
+      <ViewBooking/>
     </div>
   );
 }
